077c26b21b77255dab1bb2bd4ac44ac55062ed85
Shubhojit Dasgupta <shubhojit.dasgupta@gmail.com>
2017-04-20 17:56:02 +0530

    Add files via upload

--- MasterPages/Frontend.master.cs
+++ MasterPages/Frontend.master.cs
@@ -7,29 +7,40 @@ using System.Web.UI.WebControls;
 
 public partial class MasterPages_Frontend : System.Web.UI.MasterPage
 {
-  protected void Page_Load(object sender, EventArgs e)
-  {
-    if (!Page.IsPostBack)
+    protected void Page_Load(object sender, EventArgs e)
     {
-      string selectedTheme = Page.Theme;
-      HttpCookie preferredTheme = Request.Cookies.Get("PreferredTheme");
-      if (preferredTheme != null)
-      {
-        selectedTheme = preferredTheme.Value;
-      }
-      if (!string.IsNullOrEmpty(selectedTheme) && ThemeList.Items.FindByValue(selectedTheme) != null)
-      {
-        ThemeList.Items.FindByValue(selectedTheme).Selected = true;
-      }
+        if (!Page.IsPostBack)
+        {
+            string selectedTheme = Page.Theme;
+            HttpCookie preferredTheme = Request.Cookies.Get("PreferredTheme");
+            if (preferredTheme != null)
+            {
+                selectedTheme = preferredTheme.Value;
+            }
+            if (!string.IsNullOrEmpty(selectedTheme) && ThemeList.Items.FindByValue(selectedTheme) != null)
+            {
+                ThemeList.Items.FindByValue(selectedTheme).Selected = true;
+            }
+        }
+        switch (Page.Theme.ToLower())
+        {
+            case "darkgrey":
+                Menu1.Visible = false;
+                TreeView1.Visible = true;
+                break;
+            default:
+                Menu1.Visible = true;
+                TreeView1.Visible = false;
+                break;
+        }
     }
-  }
 
-  protected void ThemeList_SelectedIndexChanged(object sender, EventArgs e)
-  {
-    HttpCookie preferredTheme = new HttpCookie("PreferredTheme");
-    preferredTheme.Expires = DateTime.Now.AddMonths(3);
-    preferredTheme.Value = ThemeList.SelectedValue;
-    Response.Cookies.Add(preferredTheme);
-    Response.Redirect(Request.Url.ToString());
-  }
+    protected void ThemeList_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        HttpCookie preferredTheme = new HttpCookie("PreferredTheme");
+        preferredTheme.Expires = DateTime.Now.AddMonths(3);
+        preferredTheme.Value = ThemeList.SelectedValue;
+        Response.Cookies.Add(preferredTheme);
+        Response.Redirect(Request.Url.ToString());
+    }
 }
