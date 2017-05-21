618fc02272048f395be2f29ff0ee4a32a861113e
Shubhojit Dasgupta <shubhojit.dasgupta@gmail.com>
2017-05-21 18:36:55 +0530

    Email

--- Demos/Email.aspx
+++ Demos/Email.aspx
@@ -1,7 +1,11 @@
-﻿<%@ Page Title="E-mail Demo" Language="C#" MasterPageFile="~/MasterPages/Frontend.master" AutoEventWireup="true" CodeFile="Email.aspx.cs" Inherits="Demos_Email" %>
+﻿<%---Procure the App password from gmail account and use it as the value for the password
+  attribute in the network element in the configuration file. Use port 587 for sending mails 
+  through smtp.--%>
 
-<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+<%@ Page Title="E-mail Demo" Language="C#" MasterPageFile="~/MasterPages/Frontend.master" AutoEventWireup="true" CodeFile="Email.aspx.cs" Inherits="Demos_Email" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="Server">
 </asp:Content>
-<asp:Content ID="Content2" ContentPlaceHolderID="cpMainContent" Runat="Server">
+<asp:Content ID="Content2" ContentPlaceHolderID="cpMainContent" runat="Server">
 </asp:Content>
 
--- Demos/Email.aspx.cs
+++ Demos/Email.aspx.cs
@@ -8,14 +8,15 @@ using System.Net.Mail;
 
 public partial class Demos_Email : BasePage
 {
-    protected void Page_Load(object sender, EventArgs e)
-    {
-        MailMessage myMessage = new MailMessage();
-        myMessage.Subject = "Test Message";
-        myMessage.Body = "Hello world, from Planet Wox";
-        myMessage.From = new MailAddress("shubhojit.dasgupta@gmail.com", "Shubhojit Dasgupta");
-        myMessage.To.Add(new MailAddress("shubhojit.bindass@gmail.com", "Shubhojit"));
-        SmtpClient mySmtpClient = new SmtpClient();
-        mySmtpClient.Send(myMessage);
-    }
+  protected void Page_Load(object sender, EventArgs e)
+  {
+    MailMessage myMessage = new MailMessage();
+    myMessage.Subject = "Test Message";
+    myMessage.Body = "Hello world, from Planet Wox.";
+    myMessage.From = new MailAddress("shubhojit.dasgupta@gmail.com", "Shubhojit Dasgupta");
+    myMessage.To.Add(new MailAddress("shubhojit.bindass@gmail.com", "Shubhojit"));
+    SmtpClient mySmtpClient = new SmtpClient();
+    mySmtpClient.Timeout = 20000;
+    mySmtpClient.Send(myMessage);
+  }
 }
