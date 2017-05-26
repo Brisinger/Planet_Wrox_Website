be36c1874793fbded13287591bc3bf9c4f2417c7
Shubhojit Dasgupta <shubhojit.dasgupta@gmail.com>
2017-05-26 11:15:31 +0530

    Add files via upload

--- Demos/UpdatePanel.aspx.cs
+++ Demos/UpdatePanel.aspx.cs
@@ -7,8 +7,8 @@ using System.Web.UI.WebControls;
 
 public partial class Demos_UpdatePanel : BasePage
 {
-  protected void Page_Load(object sender, EventArgs e)
-  {
-    Label1.Text = System.DateTime.Now.ToString();
-  }
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Label1.Text = System.DateTime.Now.ToString();
+    }
 }
