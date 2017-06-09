6468d0f842c771ed594b88561da0fbadfe6d2171
Shubhojit Dasgupta <shubhojit.dasgupta@gmail.com>
2017-06-09 19:53:28 +0530

    Add files via upload

---
+++ App_Data/dbo.Genre.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Genre]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Name] NVARCHAR(200) NOT NULL, 
+    [SortOrder] INT NOT NULL
+)
---
+++ App_Data/dbo.Review.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Review]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Title] NVARCHAR(200) NOT NULL, 
+    [Summary] NVARCHAR(MAX) NOT NULL, 
+    [Body] NVARCHAR(MAX) NOT NULL, 
+    [GenreId] INT NOT NULL, 
+    [Authorized] BIT NOT NULL, 
+    [CreateDateTime] DATETIME NOT NULL DEFAULT getdate(), 
+    [UpdateDateTime] DATETIME NOT NULL DEFAULT getdate(), 
+    CONSTRAINT [FK_Review] FOREIGN KEY ([GenreId]) REFERENCES [Genre]([Id])
+)
